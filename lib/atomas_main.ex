04fe116852734e53ef21925d2d8f2fa5999b6507
grzegorzkemski <gregkem@gmail.com>
2016-02-04 13:03:03 +0100

    add call move function

--- lib/atomas_main.ex
+++ lib/atomas_main.ex
@@ -7,7 +7,7 @@ defmodule AtomasMain do
     import Supervisor.Spec, warn: false
     children = []
 
-    AtomasBot.hello
+    AtomasBot.hello()
 
     opts = [strategy: :one_for_one, name: AtomasMain.Supervisor]
     Supervisor.start_link(children, opts)
--- lib/atomas_move.ex
+++ lib/atomas_move.ex
@@ -1,8 +1,10 @@
 defmodule AtomasMove do
   @moduledoc false
 
-  def move(game, target) do
-    #TODO: implement
+  def callMove(gameId, target) do
+    url = "192.168.1.65/move/#{gameId}/#{target}"
+    response = HTTPotion.get url
+    response.body
   end
-  
+
 end
