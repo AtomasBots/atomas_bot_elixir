ab545a391c07d1611eb0722a42e09daa23fd6110
ordon <ordon1313@gmail.com>
2016-02-04 16:18:19 +0100

    board size for pluses :P

--- lib/bot_logic.ex
+++ lib/bot_logic.ex
@@ -6,6 +6,34 @@ defmodule BotLogic do
   end
 
   def nextMove(game) do
-    0 #insert logic here
+    nextMove(game, game.next)
+  end
+
+  def nextMove(game, next) when next == 0 do
+    addPlus(game)
+  end
+
+  def nextMove(game, next) do
+    addElement(game, next)
+  end
+
+  def addPlus(game) do
+    firstPair(game.board, 0)
+  end
+
+  def firstPair(board, index) do
+    _iterate(board, 0)
+    #if List.first(board) == List.first(List.delete_at(board, 0)), do: index + 1, else: firstPair(List.delete_at(board, 0), index + 1)
+  end
+
+  def _iterate([h|tail], acc) do
+    _iterate(tail, acc + 1)
+  end
+
+  def _iterate([],acc) do
+    acc
+  end
+  def addElement(game, next) do
+    0
   end
 end
