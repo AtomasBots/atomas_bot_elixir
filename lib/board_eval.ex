95a8f461284e543ffa5b7100d296cd0743daf7ea
ordon <ordon1313@gmail.com>
2016-02-05 11:36:52 +0100

    eval returns 2 only if plus is surround by same values

--- lib/board_eval.ex
+++ lib/board_eval.ex
@@ -10,6 +10,6 @@ defmodule BoardEval do
   end
 
   def evalPlusAtIndex(board, index) do
-    2
+    if Enum.at(board, index - 1) == Enum.at(board, index + 1), do: 2, else: 0
   end
 end
--- test/board_eval_test.exs
+++ test/board_eval_test.exs
@@ -5,7 +5,11 @@ defmodule BoardEvalTest do
     assert BoardEval.eval([1, 2, 3]) == 0
   end
 
-  test "Eval returns 2 when to ones on board" do
+  test "Eval returns 2 when two ones on board" do
     assert BoardEval.eval([1, 0, 1]) == 2
+  end
+
+  test "Eval returns 0 when plus is between 1 and 2" do
+    assert BoardEval.eval([1, 0, 2]) == 0
   end
 end
