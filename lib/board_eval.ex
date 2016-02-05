e1d245ccd7b82f3cc8a087f871870caac59e4eea
ordon <ordon1313@gmail.com>
2016-02-05 11:29:21 +0100

    eval returns 2 if 0 present

--- lib/board_eval.ex
+++ lib/board_eval.ex
@@ -2,6 +2,14 @@ defmodule BoardEval do
   @moduledoc false
 
   def eval(board) do
+    evalPlusAtIndex(board, Enum.find_index(board, fn(x) -> x == 0 end))
+  end
+
+  def evalPlusAtIndex(board, index) when index == nil do
     0
+  end
+
+  def evalPlusAtIndex(board, index) do
+    2
   end
 end
--- test/board_eval_test.exs
+++ test/board_eval_test.exs
@@ -2,6 +2,10 @@ defmodule BoardEvalTest do
   use ExUnit.Case
 
   test "Eval returns 0 for hopeless board" do
-    assert BoardEval.eval([1,2,3]) == 0
+    assert BoardEval.eval([1, 2, 3]) == 0
+  end
+
+  test "Eval returns 2 when to ones on board" do
+    assert BoardEval.eval([1, 0, 1]) == 2
   end
 end
